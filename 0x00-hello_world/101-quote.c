a342ecc7c168d968ddb8d7fadeae3b58abe878e6
Zino-Odah <zinoodah@gmail.com>
2022-06-30 20:14:05 -0700

    Advanced Tasks

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19 chars\n", "string format", 59);
 	return (1);
 }
