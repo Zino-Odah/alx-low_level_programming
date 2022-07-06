8fd9435fdd4dfb60eb071e451f5729a9bb55dc21
Zino-Odah <zinoodah@gmail.com>
2022-07-05 17:15:18 -0700

    Mandatory Tasks

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two parameters
+ *
+ * @a: parameter 1
+ * @b: parameter 2
+ *
+ * Return: result of addition
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
